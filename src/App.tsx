0d9980550dad52e13f4ae9ecdf338c1f541a9fdb
Yunu Choi <yoonwoochoi00@gmail.com>
2025-07-14 19:54:49 +1200

    feat: Vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import '@fontsource-variable/bricolage-grotesque';
 import '@fontsource-variable/inter';
 import '@fontsource-variable/schibsted-grotesk';
 import { Scene } from './pages/Scene';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
 	return (
 		<ThemeProvider theme={theme}>
+			<Analytics />
 			<CssBaseline />
 			<Scene />
 		</ThemeProvider>
